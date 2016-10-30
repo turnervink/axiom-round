0a0c729d6fbd2b41f419dc81ef105711309b5f37
turnervink <turner@turnervink.com>
2016-10-30 09:50:43 -0700

    Create all weather icon bitmaps

--- src/c/main_window.c
+++ src/c/main_window.c
@@ -3,9 +3,9 @@
 #include "main_window.h"
 
 Window *main_window;
-Layer *background_layer, *weather_icon_layer;
+Layer *background_layer;
+static BitmapLayer *weather_icon_layer;
 GFont time_font, med_font, small_font, tiny_font;
-GBitmap *weather_icons, *current_conditions;
 
 void background_update_proc(Layer *layer, GContext *ctx) {
   GRect bounds = layer_get_bounds(window_get_root_layer(main_window));
@@ -31,10 +31,8 @@ void background_update_proc(Layer *layer, GContext *ctx) {
   graphics_draw_line(ctx, GPoint(0, 180 - 60), GPoint(180, 180 - 60));
 }
 
-void weather_icon_update_proc(Layer *layer, GContext *ctx) {
-  graphics_context_set_compositing_mode(ctx, GCompOpSet);
-  current_conditions = gbitmap_create_as_sub_bitmap(weather_icons, GRect(0, 0, 16, 16));
-  graphics_draw_bitmap_in_rect(ctx, current_conditions, layer_get_bounds(weather_icon_layer));
+void weather_icon_update_proc(int code) {
+  bitmap_layer_set_bitmap(weather_icon_layer, clear_night);
 }
 
 static void main_window_load(Window *window) {
@@ -44,9 +42,23 @@ static void main_window_load(Window *window) {
   background_layer = layer_create(GRect(0, 0, bounds.size.w, bounds.size.h));
   layer_set_update_proc(background_layer, background_update_proc);
 
-  weather_icon_layer = layer_create(GRect(20, bounds.size.h / 2 - 8, 16, 16));
-  layer_set_update_proc(weather_icon_layer, weather_icon_update_proc);
+  weather_icon_layer = bitmap_layer_create(GRect(20, bounds.size.h / 2 - 8, 16, 16));
   weather_icons = gbitmap_create_with_resource(RESOURCE_ID_WEATHER_ICONS);
+
+  // Create weather icon bitmaps
+  clear_day = gbitmap_create_as_sub_bitmap(weather_icons, GRect(0, 0, 16, 16));
+  clear_night = gbitmap_create_as_sub_bitmap(weather_icons, GRect(16, 0, 16, 16));
+  cloudy = gbitmap_create_as_sub_bitmap(weather_icons, GRect(32, 0, 16, 16));
+  pcloudy_day = gbitmap_create_as_sub_bitmap(weather_icons, GRect(48, 0, 16, 16));
+  pcloudy_night = gbitmap_create_as_sub_bitmap(weather_icons, GRect(64, 0, 16, 16));
+  rain = gbitmap_create_as_sub_bitmap(weather_icons, GRect(0, 16, 16, 16));
+  thunder = gbitmap_create_as_sub_bitmap(weather_icons, GRect(16, 16, 16, 16));
+  snow = gbitmap_create_as_sub_bitmap(weather_icons, GRect(32, 16, 16, 16));
+  fog = gbitmap_create_as_sub_bitmap(weather_icons, GRect(48, 16, 16, 16));
+  unknown = gbitmap_create_as_sub_bitmap(weather_icons, GRect(64, 16, 16, 16));
+
+
+  weather_icon_update_proc(0);
 
   // Create fonts
   time_font = fonts_load_custom_font(resource_get_handle(RESOURCE_ID_TIME_FONT_48));
@@ -101,7 +113,7 @@ static void main_window_load(Window *window) {
   layer_set_frame(text_layer_get_layer(ampm_layer), GRect(bounds.size.w - (ampm_size.w + 15), bounds.size.h / 2 - ampm_size.h / 2, ampm_size.w, ampm_size.h + 5));
 
   layer_add_child(window_get_root_layer(window), background_layer);
-  layer_add_child(window_get_root_layer(window), weather_icon_layer);
+  layer_add_child(window_get_root_layer(window), bitmap_layer_get_layer(weather_icon_layer));
   layer_add_child(window_get_root_layer(window), text_layer_get_layer(time_layer));
   layer_add_child(window_get_root_layer(window), text_layer_get_layer(ampm_layer));
   layer_add_child(window_get_root_layer(window), text_layer_get_layer(date_title_layer));
--- src/c/main_window.h
+++ src/c/main_window.h
@@ -1,5 +1,6 @@
 #pragma once
 
 TextLayer *time_layer, *ampm_layer, *date_title_layer, *date_layer, *temp_title_layer, *temp_layer;
+GBitmap *weather_icons, *clear_day, *clear_night, *cloudy, *pcloudy_day, *pcloudy_night, *rain, *thunder, *snow, *fog, *unknown;
 
 void main_window_push();
